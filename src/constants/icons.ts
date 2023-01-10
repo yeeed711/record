546744488a5e20a60fc3767f71332bbc0f413bc0
yeeed711 <rladpwl910@naver.com>
2023-01-10 14:09:13 +0900

    🍱 assets: 폰트, 아이콘 추가

---
+++ src/constants/icons.ts
@@ -0,0 +1,41 @@
+import { ReactComponent as AVATAR } from '@assets/icons/avatar.svg'
+import { ReactComponent as CANCEL } from '@assets/icons/cancel.svg'
+import { ReactComponent as CHAT } from '@assets/icons/chat.svg'
+import { ReactComponent as CHECK } from '@assets/icons/check.svg'
+import { ReactComponent as CHEVRON_DOWN } from '@assets/icons/chevron_down.svg'
+import { ReactComponent as CHEVRON_LEFT } from '@assets/icons/chevron_left.svg'
+import { ReactComponent as CIRCLE_ARROW_LEFT } from '@assets/icons/circle_arrow_left.svg'
+import { ReactComponent as CIRCLE_OK } from '@assets/icons/circle_ok.svg'
+import { ReactComponent as CLOSE } from '@assets/icons/close.svg'
+import { ReactComponent as GITHUB } from '@assets/icons/github.svg'
+import { ReactComponent as HELP } from '@assets/icons/help.svg'
+import { ReactComponent as INFO } from '@assets/icons/info.svg'
+import { ReactComponent as LINK } from '@assets/icons/link.svg'
+import { ReactComponent as LOGO } from '@assets/icons/logo_record.svg'
+import { ReactComponent as MAXIMISE_FRAME } from '@assets/icons/maximise_frame.svg'
+import { ReactComponent as MOON } from '@assets/icons/moon.svg'
+import { ReactComponent as SEARCH } from '@assets/icons/search.svg'
+import { ReactComponent as SUN } from '@assets/icons/sun.svg'
+import { ReactComponent as THUMBS } from '@assets/icons/thumbs.svg'
+
+export const ICON = {
+  AVATAR,
+  CANCEL,
+  CHAT,
+  CHECK,
+  CHEVRON_DOWN,
+  CHEVRON_LEFT,
+  CIRCLE_ARROW_LEFT,
+  CIRCLE_OK,
+  CLOSE,
+  GITHUB,
+  HELP,
+  INFO,
+  LINK,
+  LOGO,
+  MAXIMISE_FRAME,
+  MOON,
+  SEARCH,
+  SUN,
+  THUMBS
+}
--- src/styles/font.ts
+++ src/styles/font.ts
@@ -4,14 +4,21 @@ export const FontCSS = css`
   @font-face {
     font-family: 'IBM Plex Sans';
     font-style: normal;
-    font-weight: regular;
+    font-weight: 400;
     src: url('@assets/fonts/IBMPlexSans-Regular.woff') format('woff');
   }
 
   @font-face {
     font-family: 'IBM Plex Sans';
     font-style: normal;
-    font-weight: bold;
+    font-weight: 500;
+    src: url('@assets/fonts/IBMPlexSans-Medium.woff') format('woff');
+  }
+
+  @font-face {
+    font-family: 'IBM Plex Sans';
+    font-style: normal;
+    font-weight: 700;
     src: url('@assets/fonts/IBMPlexSans-Bold.woff') format('woff');
   }
 `
