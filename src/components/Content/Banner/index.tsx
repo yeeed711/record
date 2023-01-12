28a4bc4ca5cd8a9f2d852766ec61e46512697a68
yeeed711 <rladpwl910@naver.com>
2023-01-12 23:04:38 +0900

    ♻️ refactor: banner, postcard 컴포넌트 분리 후 생성

---
+++ src/components/Content/Banner/index.tsx
@@ -0,0 +1,51 @@
+import LoginForm from '@components/forms/LoginForm'
+import type { ReactElement } from 'react'
+import styled from 'styled-components'
+import { useState } from 'react'
+
+const Banner = (): ReactElement => {
+  const [isSignUpModalOpened, setIsSignUpModalOpened] = useState(false)
+
+  const handleSignUpModalOpen = (): void => {
+    setIsSignUpModalOpened(prev => !prev)
+  }
+
+  return (
+    <>
+      <StyledContainer>
+        <div>생각하고, 기록하고, 성장하세요.</div>
+        <BannerBtn onClick={handleSignUpModalOpen}>시작하기</BannerBtn>
+      </StyledContainer>
+      {isSignUpModalOpened && (
+        <LoginForm setIsSignUpModalOpened={setIsSignUpModalOpened} />
+      )}
+    </>
+  )
+}
+
+export default Banner
+
+const StyledContainer = styled.div`
+  background-color: black;
+  height: 280px;
+  color: white;
+  display: flex;
+  flex-direction: column;
+  gap: 40px;
+  justify-content: center;
+  align-items: center;
+  font-size: 32px;
+  font-weight: 700;
+  background-image: url('/assets/images/banner.png');
+  background-size: cover;
+  background-position: center;
+`
+
+const BannerBtn = styled.button`
+  padding: 8px 20px;
+  color: ${props => props.theme.colors.text_03};
+  background-color: ${props => props.theme.colors.background_04};
+  border-radius: 18px;
+  border: 2px solid ${props => props.theme.colors.primary};
+  font-size: 14px;
+`
---
+++ src/components/Content/PostCards/index.tsx
@@ -0,0 +1,46 @@
+import { useEffect, useState } from 'react'
+import { mobile } from '@styles'
+import PostCard from '@components/cards/PostCard'
+import type { PostsType } from '@types'
+import type { ReactElement } from 'react'
+import styled from 'styled-components'
+import { userPostResquester } from '@api/post'
+
+const PostCards = (): ReactElement => {
+  const [userPosts, setUserPosts] = useState<PostsType>()
+
+  useEffect(() => {
+    const userPostData = async (): Promise<void> => {
+      const { post } = await userPostResquester('yezboy')
+      setUserPosts(post)
+    }
+
+    userPostData()
+  }, [])
+
+  return (
+    <StyledContainer>
+      {userPosts &&
+        userPosts.map(post => <PostCard key={post.id} post={post} />)}
+    </StyledContainer>
+  )
+}
+
+export default PostCards
+
+const StyledContainer = styled.ul`
+  display: grid;
+  grid-template-columns: repeat(3, minmax(0, 1fr));
+  gap: 25px;
+  padding: 30px 10px;
+  max-width: 1060px;
+  width: 100%;
+  margin: 0 auto;
+  ${mobile} {
+    grid-template-columns: repeat(2, minmax(0, 1fr));
+  }
+  //TODO: 미디어쿼리 분기점 추가하기
+  @media (max-width: 640px) {
+    grid-template-columns: repeat(1, minmax(0, 1fr));
+  }
+`
